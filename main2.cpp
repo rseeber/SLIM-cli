02cf69f50542f4c05885f1b4325f6046ba303847
River Seeber <riverseeber12@gmail.com>
2025-05-15 12:00:46 -0700

    working on refactor to functional and improve api

---
+++ main2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "lib/login.hpp"
+
+using namespace std;
+
+
+
+int main(int argc, char** argv){
+
+    /*
+     * We will create a number of functions which are all in one wrappers
+     * for the desired library calls. In this main() method, we will check the
+     * cli input to determine which function they want. We will call that given
+     * function by using a switch case.
+    */
+
+    
+}
